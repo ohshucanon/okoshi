e9f713561dd84844a6e183044833e30986bf1dd6
canon <ohshu.masamune@gmail.com>
2019-11-09 09:23:16 +0000

    password edit2

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -14,7 +14,7 @@
                     <input type="hidden" name="token" value="{{ $token }}">
 
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-4 control-label">登録メールアドレス</label>
+                        <label for="email" class="control-label">登録メールアドレス</label>
 
                         <div class="">
                             <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
@@ -28,7 +28,7 @@
                     </div>
 
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-md-4 control-label">新規パスワード</label>
+                        <label for="password" class="control-label">新規パスワード</label>
 
                         <div class="">
                             <input id="password" type="password" class="form-control" name="password" required>
@@ -42,7 +42,7 @@
                     </div>
 
                     <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                        <label for="password-confirm" class="col-md-4 control-label">新規パスワード(確認)</label>
+                        <label for="password-confirm" class="control-label">新規パスワード(確認)</label>
                         <div class="">
                             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
